15c942f6d377ce9fe0076b9fe3a483a0bc2f11b3
Jim Hollington <jim@bozboz.co.uk>
2016-05-18 09:52:10 +0100

    Add service provider

---
+++ src/Providers/ShippingServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bozboz\Ecommerce\Products\Providers;
+
+use Bozboz\Ecommerce\Cart\Cart;
+use Bozboz\Ecommerce\Cart\CartObserver;
+use Bozboz\Ecommerce\Checkout\Checkout;
+use Bozboz\Ecommerce\Payment\IFrameSagePayGateway;
+use Bozboz\Ecommerce\Payment\PayPalGateway;
+use Bozboz\Ecommerce\Payment\SagePayGateway;
+use Illuminate\Support\ServiceProvider;
+use Omnipay\Omnipay;
+
+class ShippingServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // $this->app->bind(
+        //     \Bozboz\Ecommerce\Products\Contracts\Product::class,
+        //     \Bozboz\Ecommerce\Products\Product::class
+        // );
+    }
+
+    public function boot()
+    {
+        $packageRoot = __DIR__ . '/../..';
+
+        $this->publishes([
+            "{$packageRoot}/database/migrations/" => database_path('migrations')
+        ], 'migrations');
+    }
+}
